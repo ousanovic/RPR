53139fca242b86134db361678ae4a2afc14738e2
ousanovic <ousanovic1@etf.unsa.ba>
2019-09-02 01:39:25 +0200

    Zavrsen zadatak

--- rpr-t1-z3/src/Main.java
+++ rpr-t1-z3/src/Main.java
@@ -2,7 +2,13 @@ public class Main {
 
     public static void main(String[] args)
     {
-        System.out.println("Hello World!");
-        System.out.println("Ubica");
+        Sat s = new Sat(15,30,45);
+        s.ispisi();
+        s.sljedeci();
+        s.ispisi();
+        s.pomjeriZa(-48);
+        s.ispisi();
+        s.postavi(0,0,0);
+        s.ispisi();
     }
 }
